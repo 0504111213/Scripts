a86a07d50379c89c432e4095ba54e5b574ceb2cc
Martin Leon <martingabriel142@gmail.com>
2026-04-06 18:53:20 -0600

    Version 0

--- menuusuario.sh
+++ menuusuario.sh
@@ -8,6 +8,48 @@ ayuda () {
 	echo "3: Cambiar password de un usuario, 4:Borrar un usuario, 5: salir"
 	echo "Nota: ejecutar script con sudo para cualquiera de las opciones"
 }
+password () {
+	while true; do
+	echo "Ingresa una contraseña para $nom_usuario: "
+	echo "La contraseña debe de tener minimo 8 caracteres, 1 mayuscula, 1 minuscula, 1 numero y 1 caracter especial"
+	read -s password
+	echo "Confirme la contraseña "
+	read -s password_confir
+
+	if test "$password" != "$password_confir"; then
+		echo "Las contraseñas no coinciden, intente de nuevo " >&2
+		continue
+	fi
+	if test ${#password} -lt 8; then 
+		echo "La contraseña debe de tener al menos 8 caracteres ">&2
+		continue
+	fi
+	if test -z "$(echo "$password" | grep '[A-Z]')"; then
+		echo "La contraseña debe de tener al menos una letra mayúscula." >&2
+		continue
+	fi
+	if test -z "$(echo "$password" | grep '[a-z]')"; then
+		echo "La contraseña debe de tener al menos una letra minúscula." >&2
+		continue
+	fi
+	if test -z "$(echo "$password" | grep '[0-9]')"; then
+		echo "La contraseña debe de tener al menos un número." >&2
+		continue
+	fi
+	if test -z "$(echo "$password" | grep '[^a-zA-Z0-9]')"; then
+		echo "Error: Debe incluir al menos un carácter especial." >&2
+		continue
+	fi
+	echo "$nom_usuario:$password" | sudo chpasswd
+	if test $? -eq 0; then
+		echo "La contraseña es segura y a sido asignada correctamente"
+		break
+	else
+		echo "Hubo un error al asignar la contraseña" >&2
+		break
+	fi
+done
+}
 pedir_usuario () {
 	echo "Ingresa nombre de usuario"
 	read nom_usuario
@@ -22,6 +64,8 @@ crear_usuario () {
 	echo "Ingresa la shell del usuario tipo: /bin/bash"
 	read shell_usu
 	sudo useradd -m -s "$shell_usu" "$nom_usuario"
+	echo "Usuario creado, ahora configura su contraseña "
+	password
 }
 agregar_usuario () {
 	pedir_usuario
@@ -31,7 +75,7 @@ agregar_usuario () {
 }
 cambiar_password () {
 	pedir_usuario
-	sudo passwd "$nom_usuario"
+	password
 }
 borrar_usuario () {
 	pedir_usuario
